5e8c38ac589a200f10e9c372ceb60babed1bbaab
Drake Axelrod <drake@draxel.io>
2021-12-20 23:09:09 +0100

    Fix for deploy

--- lib/look-at-again-when-scraping/json2handlebars.ts
+++ lib/look-at-again-when-scraping/json2handlebars.ts
@@ -1,54 +1,56 @@
-import fs from "fs";
-import Handlebars from "handlebars";
+// import fs from "fs";
+// // import Handlebars from "handlebars";
 
-const COURSES_COLUMNS = 3;
+// const COURSES_COLUMNS = 3;
 
-const PREPEND_SUMMARY_CATEGORIES = ["work", "volunteer", "awards", "publications"];
+// const PREPEND_SUMMARY_CATEGORIES = ["work", "volunteer", "awards", "publications"];
 
-function validateArray(arr: any) {
-  return (
-    arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0
-  );
-}
+// function validateArray(arr: any) {
+//   return (
+//     arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0
+//   );
+// }
 
-function render(resume: any) {
-  // Split courses into 3 columns
-  if (validateArray(resume.education)) {
-    resume.education.forEach((block: any) =>{
-      if (validateArray(block.courses)) {
-        const splitCourses: any[] = [];
-        let columnIndex = 0;
-        for (let i = 0; i < COURSES_COLUMNS; i++) {
-          splitCourses.push([]);
-        }
-        block.courses.forEach((course: any) => {
-          splitCourses[columnIndex].push(course);
-          columnIndex++;
-          if (columnIndex >= COURSES_COLUMNS) {
-            columnIndex = 0;
-          }
-        });
-        block.courses = splitCourses;
-      }
-    });
-  }
+// function render(resume: any) {
+//   // Split courses into 3 columns
+//   if (validateArray(resume.education)) {
+//     resume.education.forEach((block: any) =>{
+//       if (validateArray(block.courses)) {
+//         const splitCourses: any[] = [];
+//         let columnIndex = 0;
+//         for (let i = 0; i < COURSES_COLUMNS; i++) {
+//           splitCourses.push([]);
+//         }
+//         block.courses.forEach((course: any) => {
+//           splitCourses[columnIndex].push(course);
+//           columnIndex++;
+//           if (columnIndex >= COURSES_COLUMNS) {
+//             columnIndex = 0;
+//           }
+//         });
+//         block.courses = splitCourses;
+//       }
+//     });
+//   }
 
-  PREPEND_SUMMARY_CATEGORIES.forEach(function (category) {
-    if (resume[category] !== undefined) {
-      resume[category].forEach(function (block: any) {
-        if (block.highlights === undefined) {
-          block.highlights = [];
-        }
-      });
-    }
-  });
+//   PREPEND_SUMMARY_CATEGORIES.forEach(function (category) {
+//     if (resume[category] !== undefined) {
+//       resume[category].forEach(function (block: any) {
+//         if (block.highlights === undefined) {
+//           block.highlights = [];
+//         }
+//       });
+//     }
+//   });
 
-  const css = fs.readFileSync(`${process.cwd()}/styles/style.css`, "utf-8");
-  const tpl = fs.readFileSync(`${process.cwd()}/styles/resume.hbs`, "utf-8");
-  return Handlebars.compile(tpl)({
-    css: css,
-    resume: resume,
-  });
-}
+//   const css = fs.readFileSync(`${process.cwd()}/styles/style.css`, "utf-8");
+//   const tpl = fs.readFileSync(`${process.cwd()}/styles/resume.hbs`, "utf-8");
+//   return Handlebars.compile(tpl)({
+//     css: css,
+//     resume: resume,
+//   });
+// }
 
-export default render
+// export default render
+
+export {}
