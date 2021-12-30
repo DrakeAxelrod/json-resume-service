eefec9a5d77e1ab3beb06a719e5ae49020d3496e
Drake Axelrod <drake@draxel.io>
2021-12-30 14:19:15 +0100

    old name fix

--- pages/api/sample.ts
+++ pages/api/sample.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Req, Res } from "types";
-import { load_sample_resume } from "@utils/sample-resume-loader";
+import { load_sample_resume } from "@utils/sample-loader";
 
 const handler = async (req: Req, res: Res) => {
   const sample = load_sample_resume()
