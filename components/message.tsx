5fb2531ffa8d377f03d9ac2e9a923a07b945ca5e
metaory <metaory@gmail.com>
2024-08-28 13:36:08 +0800

    mobile fixes

--- components/message.tsx
+++ components/message.tsx
@@ -4,10 +4,9 @@ import styles from '@styles/message.module.scss'
 
 const links = { public: '/user/<username>', secret: '/gist/<gist_id>' }
 export default ({ msg, children = [], ...etc }) => {
-  console.log({ msg, children, etc })
-  const link = links[msg] ?? ''
+  const link = links[msg]
   return (
-    <Link className={styles.message} href={link} {...etc}>
+    <Link className={styles.message} href={link ?? '/'} {...etc}>
       <h3>{msg}</h3>
       {children}
       <h4>{link}</h4>
